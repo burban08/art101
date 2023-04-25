e46b7f75cd5660d184ac8b40db3918c98f5e7db2
burban08 <burban@ucsc.edu>
2023-04-25 08:12:01 -0700

    Pseudocoding
    For this lab, my partner and I made detail steps and directions for an everyday task and a game.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Dots and Boxes
+// Bianca Urban and Kelsey Melott
+// Date: April 24, 2023
 
 // Constants
 
@@ -18,4 +18,21 @@ function main() {
 }
 
 // let's get this party started
+//print updated board before each turn
+	//loop through each row
+		//loop through each space
+			//Print either dot, line - between the dots, or space between dots
+//Ask user where they want to place their line
+//place line
+//if line completes a box 
+	//color box to signify it is player’s
+//evaluate possible moves left
+	//loop through each row to see where there are lines vs. spaces
+//the computer makes a move
+	//draws line in an available space
+//returns board for player’s move
+//when there are no spaces left
+	//count number of boxes for player and computer
+	//print who won
+		//player with most boxes
 main();
