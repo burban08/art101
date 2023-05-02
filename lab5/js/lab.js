a7613c7213dbf20d7a2ccb50502c6d530d906725
burban08 <burban@ucsc.edu>
2023-05-01 19:14:34 -0700

    Lab 5 and 6
    Focused on using the data and set it in a specific array

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Bianca Urban and Kira Way
+// Date: April 26, 2023
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+var Make = "spaceship";
+var Model = "Sputnik";
+var Year = 1957;
+var Color = "white";
+var OwnIt = true;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Make: "+ Make + "<br>");
+document.writeln("Model: "+ Model + "<br>");
+document.writeln("Year: "+ Year + "<br>");
+document.writeln("Color: "+ Color + "<br>");
+document.writeln("OwnIt: "+ OwnIt + "<br>");
--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - makind a list of all transportational rides we've gone on
+// Author: Bianca Urban <burban@ucsc.edu>
+// Date: May 1, 2023
 
-// Constants
+// Define Variables
+myTransport = ["bus", "car"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// create an object for my main ride
+ var myMainRide = {
+    make: "spaceship",
+    model: "Sputnik",
+    color: "white",
+    year: 1957,
+    age: function(){
+      return 2023 - this.year;
+  }
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output  myTransport
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+//output myMainRide
+document.writeln("My main ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t', "<pre>"));
