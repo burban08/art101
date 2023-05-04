efa65ceef0ea50aab5fafb2d7b4d5fc7df59936d
burban08 <burban@ucsc.edu>
2023-05-04 14:08:36 -0700

    edits fucking bullshit

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -12,16 +12,9 @@
 function sortUserName(userName) {
   var userName = window.prompt("Hello there! Please enter your name here!");
   var nameArray = userName.split('');
-  var nameArraySort = nameArray.sort().reverse;
+  var nameArraySort = nameArray.sort().reverse();
   var nameSorted = nameArraySort.join('');
   return nameSorted;
 }
 
-function main() {
-  document.writteln("This is what I came up with using your name! Here it is: ",
-    JSON.stringify(sortUserName(), "</br>"));
-}
-
-main();
- 
-
+document.writeln("This is what I came up with using your name! Here it is: ", sortUserName(), "</br>");
