e5f4e3026e04ee2c86d758a340e3bd9dcfde3370
burban08 <burban@ucsc.edu>
2023-05-25 14:50:56 -0700

    links 2

--- site.js
+++ site.js
@@ -10,17 +10,17 @@
 
 
 $("#links").append('<ul>\
-<li><a href="../lab2/index.html">Lab 2: Hello World</a></li>\
-<li><a href="../lab3/lab3html.html">Lab 3: Lab 3: File Structure</a></li>\
-<li><a href="../lab4/index.html">Lab 4: File Transfer</a></li>\
-<li><a href="../lab5/index.html">Lab 5: Data Types and Variables</a></li>\
-<li><a href="../lab6/index.html">Lab 6: Arrays and Objects</a></li>\
-<li><a href="../lab7/index.html">Lab 7: Functions</a></li>\
-<li><a href="../lab8/index.html">Lab 8: Anon Functions and Callbacks</a></li>\
-<li><a href="../lab9/index.html">Lab 9: Javascript for the Web</a></li>\
-<li><a href="../lab10/index.html">Lab 10: Javascript Events and Forms</a></li>\
-<li><a href="../lab11/index.html">Lab 11: Libraries and jQuery</a></li>\
-<li><a href="../lab12/index.html">Lab 12: Conditionals</a></li>\
-<li><a href="../lab13/index.html">Lab 13: Conditionals</a></li>\
-<li><a href="../lab14/index.html">Lab 14: Conditionals</a></li>\
+<li><a href="/art101/lab2/index.html">Lab 2: Hello World</a></li>\
+<li><a href="/art101/lab3/lab3html.html">Lab 3: File Structure</a></li>\
+<li><a href="/art101/lab4/index.html">Lab 4: File Transfer</a></li>\
+<li><a href="/art101/lab5/index.html">Lab 5: Data Types and Variables</a></li>\
+<li><a href="/art101/lab6/index.html">Lab 6: Arrays and Objects</a></li>\
+<li><a href="/art101/lab7/index.html">Lab 7: Functions</a></li>\
+<li><a href="/art101/lab8/index.html">Lab 8: Anon Functions and Callbacks</a></li>\
+<li><a href="/art101/lab9/index.html">Lab 9: Javascript for the Web</a></li>\
+<li><a href="/art101/lab10/index.html">Lab 10: Javascript Events and Forms</a></li>\
+<li><a href="/art101/lab11/index.html">Lab 11: Libraries and jQuery</a></li>\
+<li><a href="/art101/lab12/index.html">Lab 12: Conditionals</a></li>\
+<li><a href="/art101/lab13/index.html">Lab 13: Conditionals</a></li>\
+<li><a href="/art101/lab14/index.html">Lab 14: Conditionals</a></li>\
 </ul>');
