62195eeaf1dff88254af58f489a19f0901f9813b
burban08 <burban@ucsc.edu>
2023-05-25 14:41:23 -0700

    funny links

--- site.js
+++ site.js
@@ -7,15 +7,20 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+$("#links").append('<ul>\
+<li><a href="../lab2/index.html">Lab 2: Hello World</a></li>\
+<li><a href="../lab3/lab3html.html">Lab 3: Lab 3: File Structure</a></li>\
+<li><a href="../lab4/index.html">Lab 4: File Transfer</a></li>\
+<li><a href="../lab5/index.html">Lab 5: Data Types and Variables</a></li>\
+<li><a href="../lab6/index.html">Lab 6: Arrays and Objects</a></li>\
+<li><a href="../lab7/index.html">Lab 7: Functions</a></li>\
+<li><a href="../lab8/index.html">Lab 8: Anon Functions and Callbacks</a></li>\
+<li><a href="../lab9/index.html">Lab 9: Javascript for the Web</a></li>\
+<li><a href="../lab10/index.html">Lab 10: Javascript Events and Forms</a></li>\
+<li><a href="../lab11/index.html">Lab 11: Libraries and jQuery</a></li>\
+<li><a href="../lab12/index.html">Lab 12: Conditionals</a></li>\
+<li><a href="../lab13/index.html">Lab 13: Conditionals</a></li>\
+<li><a href="../lab14/index.html">Lab 14: Conditionals</a></li>\
+</ul>');
